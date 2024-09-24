0f67724bc63afef3b5f6143f7cc5eff9ed9120a6
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2024-09-25 01:23:18 +0600

    interaction added to Main Balance

--- js/functional.js
+++ js/functional.js
@@ -1,8 +1,35 @@
 document
   .getElementById("donation-for-Noakhali")
   .addEventListener("click", function () {
-    const donateAmount = getInputFieldValue("donation-amount-1");
+    const donateAmount = getDonatedAmount("donation-amount-1");
     console.log(donateAmount);
 
-    donatedAmount("donate-balance1", donateAmount);
+    setDonatedAmount("donate-balance1", donateAmount);
+
+    // calculation Main Balance
+    setMainBalance(donateAmount);
+  });
+
+document
+  .getElementById("donation-for-Feni")
+  .addEventListener("click", function () {
+    const donateAmount = getDonatedAmount("donation-amount-2");
+    console.log(donateAmount);
+
+    setDonatedAmount("donate-balance2", donateAmount);
+
+    // calculation Main Balance
+    setMainBalance(donateAmount);
+  });
+
+document
+  .getElementById("donation-for-Quota")
+  .addEventListener("click", function () {
+    const donateAmount = getDonatedAmount("donation-amount-3");
+    console.log(donateAmount);
+
+    setDonatedAmount("donate-balance3", donateAmount);
+
+    // calculation Main Balance
+    setMainBalance(donateAmount);
   });
--- js/utilities.js
+++ js/utilities.js
@@ -1,4 +1,4 @@
-function getInputFieldValue(id) {
+function getDonatedAmount(id) {
   const inputFieldValueInString = document.getElementById(id).value;
   const donatedAmount = parseFloat(inputFieldValueInString);
 
@@ -6,9 +6,16 @@ function getInputFieldValue(id) {
   return donatedAmount;
 }
 
-function donatedAmount(id, newDonatedAmount) {
+function setDonatedAmount(id, newDonatedAmount) {
   const totalDonatedAmountField = document.getElementById(id);
   const totalDonatedAmount = totalDonatedAmountField.innerText;
   const currentAmount = parseFloat(totalDonatedAmount) + newDonatedAmount;
   totalDonatedAmountField.innerText = currentAmount;
+}
+
+function setMainBalance(donateAmount) {
+  const previousTotalBalanceField = document.getElementById("balance");
+  const previousTotalBalance = document.getElementById("balance").innerText;
+  const currentTotalBalance = parseFloat(previousTotalBalance) - donateAmount;
+  previousTotalBalanceField.innerText = currentTotalBalance;
 }
